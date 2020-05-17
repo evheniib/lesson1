82c0392984d1777320fe6678d54b87c9fa3bf649
Evhenii Beznisko <evhenii.beznysko@gmail.com>
2020-05-17 17:39:02 +0300

    First version

---
+++ hello.py
@@ -0,0 +1,20 @@
+
+
+
+def discaunted(price, discount, max_discount=55):
+    price = abs(float(price))
+    discount = abs(float(discount))
+    max_discount = abs(float(max_discount))
+    if max_discount > 99:
+        raise ValueError('To mach discount')
+    if discount >= max_discount:
+        price_with_discount = price
+    else:
+        price_with_discount = price - (price / 100 * discount)
+    return price_with_discount
+
+
+print(discaunted(100000,50))
+
+
+
